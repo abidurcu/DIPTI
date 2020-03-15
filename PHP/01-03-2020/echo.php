92c8c6afb1d10fad37c8f2fd66467f94dc88dfff
abidurcu <49652590+abidurcu@users.noreply.github.com>
2020-03-16 00:21:51 +0600

    Add files via upload

---
+++ PHP/01-03-2020/echo.php
@@ -0,0 +1,9 @@
+<?php 
+	echo "Abidur is a good boy<br>";
+	echo "He lives in mirpur<br>";
+	print "He reads in City University<br>";
+ ?>
+Department of
+<?php 
+	echo "Computer Science & Engineering";
+ ?>
---
+++ PHP/01-03-2020/variable.php
@@ -0,0 +1,7 @@
+<?php 
+	$x = 5;
+	$xyz = 15;
+	$kamal_jamal = 420;
+	$dhaka2020 = "Abidur Rahman";
+	echo $dhaka2020;
+ ?>
