12f4d486a5a1dd37b0ac0b10cb2620ab4fc7b3b4
abidurcu <49652590+abidurcu@users.noreply.github.com>
2020-03-16 00:24:11 +0600

    Add files via upload

---
+++ 67m/constant.php
@@ -0,0 +1,4 @@
+<?php 
+	define("abidur", "Abidur lives in Dhaka");
+	echo abidur;
+ ?>
---
+++ 67m/datatype.php
@@ -0,0 +1,42 @@
+<?php 
+	$x1 = "Papiya Papi";
+	var_dump($x1);
+	echo "<br>";
+
+	$x2 = 420;
+	var_dump($x2);
+	echo "<br>";
+
+	$x3 = 32.49;
+	var_dump($x3);
+	echo "<br>";
+
+	$x4 = True;
+	var_dump($x4);
+	echo "<br>";
+
+	$x5 = False;
+	var_dump($x5);
+	echo "<br>";
+
+	$x6 = null;
+	var_dump($x6);
+	echo "<br>";
+
+	$x7 = array("Dhaka","Khulna","Tangail");
+	var_dump($x7);
+	echo "<br>";
+
+	
+	class Mohakhali
+	{
+	    
+	    public function __construct()
+	    {
+	        echo null;
+	    }
+	}
+	$x8 = new Mohakhali;
+	var_dump($x8);
+	echo "<br>";
+ ?>
